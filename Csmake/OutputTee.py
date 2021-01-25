b453b88a1dbc050e80149a65403990a1b1382007
Jeremiah Patterson <jerry@casecracker.com>
2021-01-25 03:01:44 -0700

    Output on Ubuntu 20.04 with OutputTee was blank
    Fixed several issues
     - Order of closing files and deletions were incorrect
     - Flushing the actual output subsumed by tee at the end
     - Ended buffering the read file
     - Made the tread itself full owner of the read file lifecycle

--- Csmake/OutputTee.py
+++ Csmake/OutputTee.py
@@ -1,4 +1,22 @@
+# <copyright>
+# (c) Copyright 2021 Cardinal Peak Technologies, LLC
+# (c) Copyright 2020 Autumn Samantha Jeremiah Patterson
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# </copyright>
 
+import os
 import subprocess
 import sys
 import tempfile
@@ -20,38 +38,33 @@ class OutputTee:
     def subsumeStream(self, stream):
         self.actual = stream
 
-    def _consumerThread(self, myfd):
+    def _consumerThread(self, myfilename):
         buf = ""
-        while (self.threadsOpen[myfd] and self.executing) or len(buf):
-            buf = myfd.read(2048)
-            if len(buf) == 0:
-                time.sleep(.1)
-                continue
-            self.actual.write(buf)
-            self.actual.flush()
-            #time.sleep(.1)
-        else:
-            buf = myfd.read(2048)
-            while len(buf):
-                self.actual.write(buf)
+        with open(myfilename, 'r', 0) as myfd:
+            myfd.seek(0)
+            while (self.threadsOpen[myfilename] and self.executing) or len(buf):
                 buf = myfd.read(2048)
-        try:
-            myfd.close()
-        except:
-            pass
+                if len(buf) == 0:
+                    time.sleep(.1)
+                    continue
+                self.actual.write(buf)
+                self.actual.flush()
+            else:
+                buf = myfd.read(2048)
+                while len(buf):
+                    self.actual.write(buf)
+                    buf = myfd.read(2048)
 
     def _init_thread_local(self):
         self._locals.filename = "{}/{}".format(self.tempdir, threading.currentThread().getName())
-        self._locals.filebuf = open(self._locals.filename, 'wb',0)
-        self._locals.readbuf = open(self._locals.filename, 'r+b')
+        self._locals.filebuf = open(self._locals.filename, 'wb', 0)
         self._locals.currentResult = None
         self._locals.readerThread = threading.Thread(
             target=self._consumerThread,
-            args=(self._locals.readbuf, ))
+            args=(self._locals.filename, ))
         with self.lock:
-            self.threadsOpen[self._locals.readbuf] = True
+            self.threadsOpen[self._locals.filename] = True
             self.threads.append(self._locals.readerThread)
-        #self._locals.readerThread.daemon = True
         self._locals.readerThread.start()
 
     def startResult(self, result, repeat=True):
@@ -77,33 +90,36 @@ class OutputTee:
             pass
 
     def endAll(self):
-        self.executing = False
         for f in self.writers:
             try:
                 f.close()
             except:
                 pass
-        try:
-            subprocess.call(['rm', '-rf', self.tempdir])
-        except:
-            pass
+        self.executing = False
         for t in self.threads:
             try:
                 t.join()
             except:
                 pass
-
-    def close(self):
         try:
-            self.actual.flush()
+            self.actual.close()
         except:
             pass
         try:
-            self.threadsOpen[self._locals.readbuf] = False
+            "subprocess.call(['rm', '-rf', self.tempdir])"
+        except:
+            pass
+
+    def close(self):
+        try:
+            self.threadsOpen[self._locals.filename] = False
             self._locals.filebuf.close()
-            self._locals.readbuf.close()
             with self.lock:
                 f.remove(self._locals.filebuf)
+        except:
+            pass
+        try:
+            self.actual.flush()
         except:
             pass
 
